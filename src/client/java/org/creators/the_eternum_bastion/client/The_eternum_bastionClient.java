e486ff070f4d3e464d09e245ad899a6d225b1931
Sehenuk <1wgqk@vcs2go.net>
2025-03-28 12:57:49 -0400

    Initial commit

---
+++ src/client/java/org/creators/the_eternum_bastion/client/The_eternum_bastionClient.java
@@ -0,0 +1,10 @@
+package org.creators.the_eternum_bastion.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class The_eternum_bastionClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
