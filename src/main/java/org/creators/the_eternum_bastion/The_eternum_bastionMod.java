32d4ce78d796e98e3f0e3eb32d4719471ce0e2fc
CodeMaster013 <160362417+CodeMaster013@users.noreply.github.com>
2025-03-29 10:35:02 -0400

    Trying To Build

--- src/main/java/org/creators/the_eternum_bastion/The_eternum_bastionMod.java
+++ src/main/java/org/creators/the_eternum_bastion/The_eternum_bastionMod.java
@@ -2,9 +2,9 @@ package org.creators.the_eternum_bastion;
 
 import net.fabricmc.api.ModInitializer;
 
-public class The_eternum_bastion implements ModInitializer {
-
+public class The_eternum_bastionMod implements ModInitializer {
     @Override
     public void onInitialize() {
+
     }
 }
