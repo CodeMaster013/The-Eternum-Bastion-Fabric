1eb619616b80ce425eb100bf510cbd6f33e35afc
CodeMaster013 <160362417+CodeMaster013@users.noreply.github.com>
2025-03-30 10:14:35 -0400

    Trying To Get Book Working

---
+++ src/main/java/org/creators/the_eternum_bastion/The_eternum_bastionMod.java
@@ -0,0 +1,10 @@
+package org.creators.the_eternum_bastion;
+
+import net.fabricmc.api.ModInitializer;
+
+public class The_eternum_bastionMod implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
